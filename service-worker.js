5e1c50f14916c7f661398e4b6e70dc351ec1ae45
masa-oka-code <m.hill.village@gmail.com>
2026-06-28 18:38:56 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,5 @@
-const CACHE_NAME = "ds-app-cache-v2";
-const urlsToCache = [
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
@@ -20,9 +7,5 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(fetch(event.request));
 });
